b38aa43bd102efdaff5d8cd036f15e68e6d8974f
ajwires <41928847+ajwires@users.noreply.github.com>
2018-08-09 00:18:56 -0500

    first
    working. will fix it up later

---
+++ Wpf_UART/Home.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+
+namespace Wpf_UART
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        private SerialPort serial;  //serial port to send to uart page
+
+        public Home()
+        {
+            InitializeComponent();
+            serial = new SerialPort();
+
+            //show list of valid com ports
+            if (SerialPort.GetPortNames().Length != 0)
+            {
+                btnSelect.IsEnabled = true;
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    portsListBox.Items.Add(s);
+                }
+                //generate ports list
+            }
+        }
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            // set serial
+            serial_init();
+            //change page
+            UART uartPage = new UART(serial);
+            this.NavigationService.Navigate(uartPage);
+        }
+
+        //setup serial port. should probably do this somewhere else.
+        private void serial_init()
+        {
+            string selectedPort = portsListBox.SelectedItem.ToString();
+            Int32 baudRate = Convert.ToInt32(baudsComboBox.Text);
+            serial.PortName = selectedPort; //Com Port Name                
+            serial.BaudRate = baudRate; //COM Port Sp
+            serial.Handshake = System.IO.Ports.Handshake.None;
+            serial.Parity = Parity.None;
+            serial.DataBits = 8;
+            serial.StopBits = StopBits.One;
+            serial.ReadTimeout = 200;
+            serial.WriteTimeout = 50;
+        }
+
+    }
+}
---
+++ Wpf_UART/UART.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Windows.Threading;
+using System.Threading;
+
+namespace Wpf_UART
+{
+    /// <summary>
+    /// Interaction logic for UART.xaml
+    /// </summary>
+    public partial class UART : Page
+    {
+        private SerialPort serial;
+
+        public UART(SerialPort rxPort)
+        {
+            InitializeComponent();
+            serial = rxPort;
+            //set serial recieve function
+            serial.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(Recieve);
+            serial.Open();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            //get data from text box
+            String data = uartTxTextBox.Text.ToString();
+            Send(data);
+        }
+        //send serial data
+        private void Send(String data)
+        {
+            if (serial.IsOpen)
+            {
+                try
+                {
+                    // Send the binary data out the port
+                    byte[] hexstring = Encoding.ASCII.GetBytes(data);
+                    foreach (byte hexval in hexstring)
+                    {
+                        byte[] _hexval = new byte[] { hexval };     // need to convert byte 
+                                                                    // to byte[] to write
+                        serial.Write(_hexval, 0, 1);
+                        Thread.Sleep(1);
+                    }
+                    txListBox.Items.Add(data);  //add to data sent.
+                }
+                catch (Exception ex)
+                {
+                    /*para.Inlines.Add("Failed to SEND" + data + "\n" + ex + "\n");
+                    mcFlowDoc.Blocks.Add(para);
+                    Commdata.Document = mcFlowDoc;*/
+                }
+            }
+        }
+        //recieve data thread
+        private void Recieve(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            // Collecting the characters received to our 'buffer' (string).
+            var recieved_data = serial.ReadExisting();
+            //dispatcher to the UI thread
+            Dispatcher.Invoke(() =>
+            { 
+                // update text box
+                rxListBox.Items.Add(recieved_data);
+            });
+
+        }
+    }
+}
